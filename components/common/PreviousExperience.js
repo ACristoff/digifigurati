ec7c14d2f34bb8cfced84b15c26aea09570215f8
Alex Cristoffanini <acristoffanini@gmail.com>
2023-03-02 14:42:49 -0800

    previous exp component

--- components/common/PreviousExperience.js
+++ components/common/PreviousExperience.js
@@ -1,11 +1,29 @@
-import { Box } from '@mui/material'
+import { Box, Stack, Typography } from '@mui/material'
 import Image from 'next/image'
 import React from 'react'
 
 const PreviousExperience = () => {
   return (
-    <Box>
-        <Image />
+    <Box sx={{display: 'flex', 'flex-direction': 'column', alignItems: 'center'}}>
+        <Typography variant='h2'>Previous Experience</Typography>
+        <Stack direction='row' spacing={4}>
+            <Image 
+                src="/images/ProAceLogo.jpg"
+                height={100}
+                width={350}
+            />
+            <Image 
+                src="/images/ProAceLogo.jpg"
+                height={100}
+                width={300}
+            />
+            <Image 
+                src="/images/ProAceLogo.jpg"
+                height={100}
+                width={300}
+            />
+        </Stack>
+
     </Box>
   )
 }
